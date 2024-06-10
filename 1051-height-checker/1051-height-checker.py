c9523745bf9a2603cedd4dc0ac0a3b2b8975a078
Pranshu Raj <113211320+pranshu-raj-211@users.noreply.github.com>
2024-06-11 00:07:38 +0530

    Time: 38 ms (64.91%), Space: 16.5 MB (77.64%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,8 +1,3 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        expected = sorted(heights)
-        deviations = 0
-        for i in range(len(heights)):
-            if heights[i]!=expected[i]:
-                deviations+=1
-        return deviations
+        return sum(h1!=h2 for h1, h2 in zip(heights, sorted(heights)))
